66202eb5bea81e1e8097e48e8fe746725d957b0b
Aviz85 <avizmaeir@gmail.com>
2024-09-01 19:39:50 +0300

    pip freeze

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from app import create_app
 
-app = Flask(__name__)
-CORS(app)
-
-# Serve React App
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
-
-# Add your API routes here
-# For example:
-@app.route('/api/example', methods=['GET'])
-def example_route():
-    return jsonify({"message": "This is an example API route"})
+app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
